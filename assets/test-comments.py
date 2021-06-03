dc14d173023044b328def194a3d758ee2e8b1ab5
Jesse <jr137@lab-rg07-15.cms.waikato.ac.nz>
2021-06-03 13:51:05 +1200

    Fix static comment convention verification test to when comparing currently line with convention preset line

--- assets/test-comments.py
+++ assets/test-comments.py
@@ -3,7 +3,7 @@ import os
 import re
 
 # Declare comment convention
-comments = ["// Name: Jesse Reyneke-Barnard\n", "// ID: 1351388\n"]
+comments = ["// Name                    ID\n", "// Jesse Reyneke-Barnard   1351388\n"]
 
 # Compile file regular expression pattern
 pattern = re.compile("^.*\.ts$")
@@ -11,15 +11,18 @@ pattern = re.compile("^.*\.ts$")
 for root, dirs, files in os.walk("./src"):
     for name in files:
         if (pattern.match(name)):
-            print("test-comments -> info -> checking " + os.path.join(root, name))
+            print("test-comments -> info -> checking file: \"" + os.path.join(root, name) + "\"")
             # If the correct file was found, check it for correct comment header convention
             f = open(os.path.join(root, name)) # Open file
             l = f.readline() # Read initial line
             for c in comments:
                 if (c != l):
-                    print("test-comments -> fail -> file: " + os.path.join(root, name) + " failed with line: " + l)
+                    print("test-comments -> fail -> file: \"" + os.path.join(root, name) + "\"")
+                    print("                         line: \"" + l.encode('unicode_escape').decode('ASCII') + "\"")
+                    print("                     expected: \"" + c.encode('unicode_escape').decode('ASCII') + "\"")
                     sys.exit(1)
                 else:
-                    l = f.readLine()
+                    l = f.readline()
+            print("test-comments -> pass -> file: \"" + os.path.join(root, name) + "\" matches comment header convention")
 print("test-comments -> pass -> all files had the correct comment format")
 sys.exit(0)
